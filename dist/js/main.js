fad6f4fd667d94001844d96fc879b2b061b16cab
Ifeoluwa Olaiya <ifeoluwa1201@gmail.com>
2021-06-20 04:31:43 +0100

    changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,4 +6,25 @@ navList.style.maxHeight = '0px';
 menuIcon.addEventListener('click', function () {
     if (navList.style.maxHeight == '0px') navList.style.maxHeight = '200px';
     else navList.style.maxHeight = '0px';
-})
+});
+
+
+//Product Image
+const productImage = document.querySelector('#product-img');
+const smallImage = document.querySelectorAll('.small-img');
+
+smallImage[0].addEventListener('click', function () {
+    productImage.src = smallImage[0].src;
+});
+
+smallImage[1].addEventListener('click', function () {
+    productImage.src = smallImage[1].src;
+});
+
+smallImage[2].addEventListener('click', function () {
+    productImage.src = smallImage[2].src;
+});
+
+smallImage[3].addEventListener('click', function () {
+    productImage.src = smallImage[3].src;
+});
